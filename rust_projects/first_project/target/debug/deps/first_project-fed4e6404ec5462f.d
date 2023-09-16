ad184ba67353860c2a074b7849299e0c2780e634
Diego Ramos <drramos25@tamu.edu>
2023-09-16 18:37:07 -0500

    finished hello world, started new project

---
+++ rust_projects/first_project/target/debug/deps/first_project-fed4e6404ec5462f.d
@@ -0,0 +1,5 @@
+/home/diego/Documents/Coding_Projects/rust_projects/first_project/target/debug/deps/first_project-fed4e6404ec5462f: src/main.rs
+
+/home/diego/Documents/Coding_Projects/rust_projects/first_project/target/debug/deps/first_project-fed4e6404ec5462f.d: src/main.rs
+
+src/main.rs:
---
+++ rust_projects/first_project/target/debug/first_project.d
@@ -0,0 +1,1 @@
+/home/diego/Documents/Coding_Projects/rust_projects/first_project/target/debug/first_project: /home/diego/Documents/Coding_Projects/rust_projects/first_project/src/main.rs
