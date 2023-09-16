3c20580104a69ed896e494c3a74ab7b541a8be9a
Diego Ramos <drramos25@tamu.edu>
2023-09-16 13:02:38 -0500

    rust guessing game completed

--- rust_projects/guessing_game/src/main.rs
+++ rust_projects/guessing_game/src/main.rs
@@ -1,22 +1,40 @@
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 // this is a comment
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number = rand::thread_rng().gen_range(1..=100); //generate a random number between 1-100 inclusive
 
     println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
+    loop { //loop until correct guess
 
-    let mut guess = String::new(); //'mut' makes a variable mutable
+        println!("Please input your guess.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new(); //'mut' makes a variable mutable
 
-    println!("You guessed: {guess}");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        }; //shadow guess and turn into a number
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) { //compare guess to secret number
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break; //end when correct guess
+            }
+        }
+    }
 }
 
