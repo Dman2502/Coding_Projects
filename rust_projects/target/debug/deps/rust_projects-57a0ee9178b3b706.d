09b3b2f2f2ab94b916aa85b2de8f5a0da0508850
Diego Ramos <drramos25@tamu.edu>
2023-09-15 23:25:18 -0500

    rust tutorial

---
+++ rust_projects/target/debug/deps/rust_projects-57a0ee9178b3b706.d
@@ -0,0 +1,5 @@
+/home/diego/Documents/Coding_Projects/rust_projects/target/debug/deps/rust_projects-57a0ee9178b3b706: src/main.rs
+
+/home/diego/Documents/Coding_Projects/rust_projects/target/debug/deps/rust_projects-57a0ee9178b3b706.d: src/main.rs
+
+src/main.rs:
---
+++ rust_projects/target/debug/rust_projects.d
@@ -0,0 +1,1 @@
+/home/diego/Documents/Coding_Projects/rust_projects/target/debug/rust_projects: /home/diego/Documents/Coding_Projects/rust_projects/src/main.rs
