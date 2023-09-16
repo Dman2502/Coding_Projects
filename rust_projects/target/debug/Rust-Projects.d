b2eb99205ff4d55e13e559b14f4d7ad46628aad4
Diego Ramos <drramos25@tamu.edu>
2023-09-15 22:52:39 -0500

    more rust setup

---
+++ rust_projects/target/debug/Rust-Projects.d
@@ -0,0 +1,1 @@
+/home/diego/Documents/Coding_Projects/rust_projects/target/debug/Rust-Projects: /home/diego/Documents/Coding_Projects/rust_projects/src/main.rs
---
+++ rust_projects/target/debug/deps/Rust_Projects-d2551b99485eddcc.d
@@ -0,0 +1,5 @@
+/home/diego/Documents/Coding_Projects/rust_projects/target/debug/deps/Rust_Projects-d2551b99485eddcc: src/main.rs
+
+/home/diego/Documents/Coding_Projects/rust_projects/target/debug/deps/Rust_Projects-d2551b99485eddcc.d: src/main.rs
+
+src/main.rs:
