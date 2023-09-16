81008718b2d3088732876ae63d55430333fbb2a9
Diego Ramos <drramos25@tamu.edu>
2023-09-15 22:50:35 -0500

    setting up rust

---
+++ rust_projects/target/debug/deps/Rust_Projects-045569bdcd52461b.d
@@ -0,0 +1,5 @@
+/home/diego/Documents/Coding_Projects/Rust-Projects/target/debug/deps/Rust_Projects-045569bdcd52461b.rmeta: src/main.rs
+
+/home/diego/Documents/Coding_Projects/Rust-Projects/target/debug/deps/Rust_Projects-045569bdcd52461b.d: src/main.rs
+
+src/main.rs:
---
+++ rust_projects/target/debug/deps/Rust_Projects-9fa14ec0a27a88c1.d
@@ -0,0 +1,5 @@
+/home/diego/Documents/Coding_Projects/Rust-Projects/target/debug/deps/Rust_Projects-9fa14ec0a27a88c1.rmeta: src/main.rs
+
+/home/diego/Documents/Coding_Projects/Rust-Projects/target/debug/deps/Rust_Projects-9fa14ec0a27a88c1.d: src/main.rs
+
+src/main.rs:
