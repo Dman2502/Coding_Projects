971406a1c2dd853728da9315e0af7a8ff82c22f3
Diego Ramos <drramos25@tamu.edu>
2023-09-17 22:36:05 -0500

    working on weird algorithm

--- rust_projects/weird_algorithm/src/main.rs
+++ rust_projects/weird_algorithm/src/main.rs
@@ -7,19 +7,22 @@ use std::io;
 // ex. input "3" 3 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
 
 fn main() {
-    println!("Enter a positive number between 1 and 10^6");
-
-    let mut n = String::new();
+    println!("Weird Algorithm!");
+    
+    // create 
+    let mut input_n = String::new();
 
     io::stdin()
-        .read_line(&mut n)
+        .read_line(&mut input_n)
         .expect("Failed to read line");
 
+    println!("You entered: {input_n}");
+    
+    // let sequence: [u32; 0] = [];
+    // uncomment this^ when you figure it out
+    // use a loop to check if even, odd, or equal to 1
+    // append each term to an array then print the array
+    loop {
 
-    // Turn n into a number
-    // let n: u32 = match n.trim().parse() {
-    //     Ok(num) => num,
-    //     Err(_) => break,
-    // };
-    println!("You entered: {n}");
+    }
 }
