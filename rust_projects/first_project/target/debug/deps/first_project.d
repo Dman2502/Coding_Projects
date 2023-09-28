2391b6be2feed94c257d329582fb858e04eba65f
Dman2502 <drramos25@tamu.edu>
2023-09-28 15:51:41 -0500

    fixed things on windows

---
+++ rust_projects/first_project/target/debug/deps/first_project.d
@@ -0,0 +1,5 @@
+C:\Users\dscou\.vscode\Coding_Projects\rust_projects\first_project\target\debug\deps\first_project.exe: src\main.rs
+
+C:\Users\dscou\.vscode\Coding_Projects\rust_projects\first_project\target\debug\deps\first_project.d: src\main.rs
+
+src\main.rs:
